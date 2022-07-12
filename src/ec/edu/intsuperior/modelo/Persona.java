bef0a7ba201bf941ee85fc7271b3f35063ed9567
cristian.guatemal <cristian.guatemal@iess.gob.ec>
2022-07-12 17:37:38 -0500

    Esta es la version de cquinchiguango v2

--- src/ec/edu/intsuperior/modelo/Persona.java
+++ src/ec/edu/intsuperior/modelo/Persona.java
@@ -1,14 +1,124 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.intsuperior.modelo;
 
-/**
- *
- * @author Windows 10
- */
 public class Persona {
+
+    private String nombre1;
+    private String nombre2;
+    private String ci;
+    private String apellido1;
+    private String apellido2;
+    private String celular;
+    private String direccion;
+    private String correo;
+    private String fechaNacimiento;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre1, String nombre2, String ci, String apellido1, String apellido2, String celular, String correo, String fechaNacimiento) {
+        this.nombre1 = nombre1;
+        this.nombre2 = nombre2;
+        this.ci = ci;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.celular = celular;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    public void setNombre2(String nombre2) {
+        this.nombre2 = nombre2;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    public String getNombre2() {
+        return nombre2;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public int calcularedad() {
+        int edad = 23;
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Los tipos de datos son: \n "
+                + "cedula: " + getApellido1() + "\n"
+                + "apellidos y nombres : " + getApellido1() + " " + getApellido2()
+                + " " + getNombre1() + " " + getNombre2() + "\n"
+                + "Direcion: " + getDireccion() + "\n"
+                + "Celular: " + getCelular() + "\n"
+                + "Correo: " + getCorreo() + "\n"
+                + "Edad: " + calcularedad();
+
+    }
+
     
-}
+
+    }
+
+
---
+++ src/ec/edu/intsuperior/vistas/Aplicacion.java
@@ -0,0 +1,21 @@
+package ec.edu.intsuperior.vistas;
+import ec.edu.intsuperior.modelo.Persona;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        Scanner leer= new Scanner(System.in);
+        Persona p1= new Persona();
+        System.out.println("Ingresa la cedula");
+        p1.setCi (leer.next());
+        System.out.println("Ingresa nombre 1");
+        p1.setNombre1(leer.next());
+        p1.setNombre2(JOptionPane.showInputDialog("Ingresa tu  segundo nombre "));
+        p1.setApellido1(JOptionPane.showInputDialog("ingresa tu apellido paterno "));
+        
+        JOptionPane.showMessageDialog(null,p1.toString());  
+
+    }
+}
