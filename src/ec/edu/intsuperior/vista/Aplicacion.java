50f0b75e30c77a6db232f5db7aed661699b8be80
DELL <DELL@DESKTOP-R5UV8LU>
2022-07-12 15:14:06 -0500

    version 2 de lida puchaicela

--- src/ec/edu/intsuperior/vista/Aplicacion.java
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -5,10 +5,31 @@
  */
 package ec.edu.intsuperior.vista;
 
+import ec.edu.intsuperior.modelo.Persona;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Windows 10
  */
 public class Aplicacion {
-    
+    public static void main(String[] args) {
+        Scanner leer= new Scanner (System.in);
+        Persona p1= new Persona ();
+        System.out.println("ingrese el nombre 1");
+        p1.setNombre1(leer.next());
+        System.out.println("ingrese numero cedula");
+        
+        System.out.println("ingrese el nombre 2");
+        p1.setNombre2(leer.next());
+        System.out.println("ingrese el apellido 1");
+        p1.setApellido1(leer.next());
+        System.out.println("ingrese el apellido 2");
+        p1.setApellido2(leer.next());
+        
+        JOptionPane.showMessageDialog(null, p1.toString());
+        
+        
+    }
 }
